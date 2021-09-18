aee86318fbf61ac8e096c0ad5b17d976eb7e6b0f
Abu Faiz <a.faiz.2614@gmail.com>
2021-09-18 09:09:06 +0530

    changes made

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const connectToMongoose = require('./db')
 const express = require('express')
-
+var cors = require('cors')
 
 connectToMongoose();
 
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
 
 //Available Routes
